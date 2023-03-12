be5dda010f1891f716c2b72d6e089b63665d65b6
simon <simonkhartoum@gmail.com>
2023-03-12 09:01:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(people,event){
+    const watu =[];
+    for(let i=0; i<people.length; i++){
+        watu.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+    }
+    return watu
+}
+
+function countDown (num){
+
+    while(num >= 0 ){
+        console.log (num);
+       num--;
+    }
+}
